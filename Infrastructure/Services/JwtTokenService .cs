2b97f7a3ec456fc98f2e96da3d4767b5fb41a005
Excellent Selekane <bajoselekane@gmail.com>
2026-02-06 04:34:09 +0200

    Application structure

--- Infrastructure/Services/JwtTokenService .cs
+++ Infrastructure/Services/JwtTokenService .cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Domain.Entities
+namespace Infrastructure.Services
 {
-    public class ApplicationUser
+    public class JwtTokenService
     {
     }
 }
---
+++ PHCIS-App/PHCIS-App.Web/Controller/AuthenticationController.cs
@@ -0,0 +1,37 @@
+﻿using Infrastructure.DTOs;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32;
+
+namespace PHCISApp.Web.Controller
+{
+ [Route("api/[controller]")]
+[ApiController]
+    public class AuthenticationController(IUserAccount accountInterface) : ControllerBase
+    {
+        [HttpPost("register")]
+
+        public async Task<IActionResult> CreateAsync(Register user)
+        {
+            if (user == null) return BadRequest("Model is created");
+            var result = await accountInterface.CreateAsync(user);
+            return Ok(result);
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> SignInAsync(Login user)
+        {
+            if (user == null) return BadRequest("Model is Empty");
+            var result = await accountInterface.SignInAsync(user);
+            return Ok(result);
+        }
+        [HttpPost("refresh-token")]
+        public async Task<IActionResult> RefreshTokenAsync(RefreshToken token)
+        {
+            if (token == null) return BadRequest("Token is Empty");
+            var result = await accountInterface.RefreshTokenAsync(token);
+            return Ok(result);
+        }
+    }
+
+}
