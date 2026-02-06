327484fae34e931063a88e89c9db3cfc8ad29051
Excellent Selekane <bajoselekane@gmail.com>
2026-02-06 02:14:14 +0200

    Clean Architecture structure

---
+++ Domain/Entities/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities.Enums;
+
+
+namespace Domain.Entities
+{
+    public class BaseEntity
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public bool IsDeleted { get; set; } = false;
+        public DateTime? DeletedAt { get; set; }
+        public DateTime CreatedAt { get; set; }= DateTime.Now;
+        public DateTime? UpdatedAt { get; set; }
+        public  string? CreatedBy { get; set; }
+        public string? UpdatedBy { get; set; }
+        public EntityStatus Status { get; set; }
+
+    }
+}
---
+++ Domain/Entities/Enums/EntityEnums.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Domain.Entities.Enums
+{
+    public enum EntityStatus
+    {
+        Confirmed = 1,
+        Pending = 2,
+        Declined = 3
+    }
+}
